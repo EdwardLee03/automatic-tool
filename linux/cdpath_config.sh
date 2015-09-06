4d625ee21bff1cf4c9dadc8bc87651320c2d3e7c
Bert Lee <bert825_work@163.com>
2015-09-06 15:43:48 +0800

    [Linux] Hack 1. Define CD Base Directory Using CDPATH.

---
+++ linux/cdpath_config.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------
+# Hack 1. Define CD Base Directory Using CDPATH.
+#
+# Example:
+#    sh cdpath_config.sh -p '.:~:~/Documents/workspace:~/Documents/workspace/GitHub:~/Documents/Tech:~/Documents/Mogu'
+#
+# User: xingle
+# Date: 15/9/6
+# Time: 14:37
+# -----------------------------------------------
+
+usage () {
+    echo "Usage:"
+    echo ""
+    echo "  -p  parent directory that cd to subdirectories"
+}
+
+cd_path='.:~'
+while getopts ":p:h" opt; do
+    case "$opt" in
+    p)
+        cd_path="$OPTARG"
+        ;;
+    h)
+        usage
+        exit 2
+        ;;
+    ?)
+        usage
+        exit 2
+        ;;
+    esac
+done
+
+if [ -z "$cd_path" ]; then
+    echo "Failed: -p param must be not empty!";
+    exit 3
+fi
+
+profile="$HOME/.profile"
+echo '' >> "$profile"
+echo '# define CD base directory' >> "$profile"
+echo "export CDPATH=$cd_path" >> "$profile"
+
+# reload User's Profile
+[ -r "$profile" ] && . "$profile"
