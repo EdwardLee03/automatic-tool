50a602168b34061e4bb6216a9d418ca3069231bd
Bert Lee <bert825_work@163.com>
2015-09-07 11:02:19 +0800

    [Linux] Config JDK environment.

---
+++ linux/jdk_config.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------
+# Config JDK environment.
+#
+# Example:
+#    sh jdk_config.sh -v '1.8.0_60'
+#
+# User: Bert Lee
+# Date: 2015/9/2
+# Time: 16:42
+# -----------------------------------------------
+
+usage () {
+    echo "Usage:"
+    echo ""
+    echo "  -v  JDK version for used"
+}
+
+jdk_version=''
+while getopts ":v:h" opt; do
+    case "$opt" in
+    v)
+        jdk_version="$OPTARG"
+        ;;
+    h)
+        usage
+        exit 2
+        ;;
+    ?)
+        usage
+        exit 2
+        ;;
+    esac
+done
+
+if [ -z "$jdk_version" ]; then
+    echo "Failed: -v param must be not empty!";
+    exit 3
+fi
+
+java_home=/Library/Java/JavaVirtualMachines/jdk"$jdk_version".jdk/Contents/Home
+
+dev_env="$HOME/.dev_env"
+echo '' >> "$dev_env"
+echo '# config JDK' >> "$dev_env"
+echo "export JAVA_HOME=$java_home" >> "$dev_env"
+echo 'export PATH=$JAVA_HOME:$PATH' >> "$dev_env"
+
+# reload User's Profile
+profile="$HOME/.profile"
+[ -r "$profile" ] && . "$profile"
+#[ -r ~/.profile ] && . ~/.profile
+
+[ $? -eq 0 ] && echo "set JAVA_HOME to '$java_home'"
--- mac/bash_env_init.sh
+++ mac/bash_env_init.sh
@@ -14,13 +14,13 @@
 PRG="$0"
 
 while [ -h "$PRG" ] ; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "$PRG"`/"$link"
-  fi
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`/"$link"
+    fi
 done
 
 # 执行程序所在的根目录
